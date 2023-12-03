4a32216abdbdc5e312d792eecf648f48368b35ff
cndnhere <98242648+cndnhere@users.noreply.github.com>
2023-12-03 11:13:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,47 @@
-const express = require("express");
+// server.js
+const express = require('express');
+const range = require('range-parser');
+const path = require('path');
+const fs = require('fs');
+
 const app = express();
-const fs = require("fs");
-app.use((req, res, next) => {
-    console.log('Requested URL:', req.url);
-    console.log('Request IP:', req.ip);
-    if(req.ip == '::ffff:127.0.0.1') 
-    {
-        res.status(400);
-    }
-    console.log('Request from:', req.headers['user-agent']); // Accessing user-agent header
-    console.log('Request IP:', req.ip); // Accessing IP address of the requester
-    next();
-  });
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
+const port = 3000;
+
+const videoPath = './sample_video.mp4';
+const chunkSizeKB = 1024; // 1 KB (adjust as needed)
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get("/video", function (req, res) {
+app.get('/video', (req, res) => {
     const range = req.headers.range;
     if (!range) {
-        res.status(400).send("Requires Range header");
+      res.status(400).send('Range header is required');
+      return;
     }
-    const videoPath = "./sample_video.mp4";
+  
     const videoSize = fs.statSync(videoPath).size;
-    // console.log("size of video is:", videoSize);    
-    const CHUNK_SIZE = 10 ** 6; //1 MB    
-    const start = Number(range.replace(/\D/g, ""));
-    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+    const chunkSize = 10 * 1024; // Chunk size in KB (adjust as needed)
+    const start = Number(range.replace(/\D/g, ''));
+    const end = Math.min(start + chunkSize, videoSize - 1);
+  
     const contentLength = end - start + 1;
     const headers = {
-        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-        "Accept-Ranges": 'bytes',
-        "Content-Length": contentLength,
-        "Content-Type": "video/mp4"
-    }
+      'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': contentLength,
+      'Content-Type': 'video/mp4',
+    };
+  
     res.writeHead(206, headers);
+  
     const videoStream = fs.createReadStream(videoPath, { start, end });
     videoStream.pipe(res);
-});
+  });
 
-app.listen(3000, function () {
-    console.log("Server is running on port:", 3000);
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });

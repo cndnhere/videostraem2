ca94174c96915cdf8761d024557a82a14b90d3e2
cndnhere <98242648+cndnhere@users.noreply.github.com>
2023-12-05 14:44:34 +0530

    Update index.js
    asdasdasd

--- index.js
+++ index.js
@@ -1,56 +1,58 @@
-// server.js
 const express = require('express');
-const range = require('range-parser');
-const path = require('path');
-const fs = require('fs');
-
 const app = express();
-const port = 3000;
+const fs = require('fs');
 isUsed = 1;
-const videoPath = './sample_video.mp4';
-const chunkSizeKB = 1024; // 1 KB (adjust as needed)
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.get('/video', (req, res) => {
-    const range = req.headers.range;
-    if (!range) {
-      res.status(400).send('Range header is required');
-      return;
-    }
+
+app.get('/video/:id', function (req, res) {
+  console.log(isUsed);
+  const range = req.headers.range;
+  console.log(range);
+  if (!range) {
+    res.status(400);
+    // res.send('')
+  }
   if ((isUsed == 1 &&  (range== undefined  || range == "bytes=0-")) || (isUsed==0 && range != "bytes=0-" && range!= undefined  )) {
-     isUsed = 0;
-      const videoSize = fs.statSync(videoPath).size;
+    isUsed = 0;
+    const videoPath = './sample_video.mp4';
+    //const videoPath  = "https://www.youtube.com/watch?v=Z4Jg_eEDEQg";
+    const stat = fs.statSync(videoPath);
+    const fileSize = stat.size;
+
     if (range) {
-         const chunkSize = 10 * 1024; // Chunk size in KB (adjust as needed)
-    const start = Number(range.replace(/\D/g, ''));
-    const end = Math.min(start + chunkSize, videoSize - 1);
-  
-    const contentLength = end - start + 1;
-    const headers = {
-      'Content-Range': `bytes ${start}-${end}/${videoSize}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': contentLength,
-      'Content-Type': 'video/mp4',
-    };
-  
-    res.writeHead(206, headers);
-  
-    const videoStream = fs.createReadStream(videoPath, { start, end });
-    videoStream.pipe(res);
+      const parts = range.replace(/bytes=/, "").split("-");
+      const start = parseInt(parts[0], 10);
+      const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+
+      const chunksize = (end - start) + 1;
+      const file = fs.createReadStream(videoPath, { start, end });
+      const head = {
+        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': chunksize,
+        'Content-Type': 'video/mp4',
+      };
+
+      res.writeHead(206, head);
+      file.pipe(res);
+    } else {
+      const head = {
+        'Content-Length': fileSize,
+        'Content-Type': 'video/mp4',
+      };
+      res.writeHead(200, head);
+      fs.createReadStream(videoPath).pipe(res);
+      //res.status({ isContinue: 1 });
     }
-  }else{
+  } else {
     console.log("Invalid");
     res.status("Invalid");
   }
-    
- 
-  });
+});
 
-app.use(express.static('public'));
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });

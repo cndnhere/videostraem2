5a8eb4ebef673b1b7ec7d562e0a529ac2b7d9ecd
cndnhere <98242648+cndnhere@users.noreply.github.com>
2024-01-10 16:04:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,19 +39,46 @@ app.get('/stream/:token/:videoId', async (req, res) => {
         // First API call
         const response1 = await axios.get('https://video-stream-server-z3gs.onrender.com/api/VideoPlayerInfo/GetByToken/' + req.params.token);
         objVideoInfo = response1.data;
+        // console.log('Get isplay '+objVideoInfo.isPlay);
 
-        isPlay = objVideoInfo? objVideoInfo.isPlay:0;
+        isPlay = objVideoInfo?objVideoInfo.isPlay:0;
         if (isPlay == 0) {
             let obj = {
                 isPlay: 1
             }
             const response2 = await axios.put('https://video-stream-server-z3gs.onrender.com/api/VideoPlayerInfo/Update/' + req.params.token, obj);
             objInfoUpdate = response2.data;
+            console.log(objInfoUpdate);
         }
 
         const range = req.headers.range;
+        console.log(isPlay);
         console.log(range);
-        ytdl('https://www.youtube.com/watch?v=79dq08VydIo').pipe(res);
+       // if ((isPlay == 0 && (range == undefined || range == "bytes=0-")) || (isPlay == 1 && range != undefined)) {
+         if ((isPlay == 0 && range == "bytes=0-") || (isPlay == 1 && (range != "bytes=0-" &&  range != undefined))) {
+            const videoURL = 'https://youtube.com/shorts/' + getVideo(req.params.videoId);
+             // ytdl('http://www.youtube.com/watch?v=zs6T4Bq9uUs').pipe(res);
+            const stream = ytdl(videoURL, { filter: 'audioandvideo', quality: 'highest' });
+
+            // console.log(stream);
+
+            // Create FFmpeg process
+            const ffmpegProcess = ffmpeg(stream)
+                .format('mp4')
+                .audioCodec('copy')
+                .videoCodec('copy')
+                .outputOptions('-movflags frag_keyframe+empty_moov')
+                .on('error', (err) => {
+                    console.error('Error occurred: ' + err.message);
+                })
+                .on('end', () => {
+                    console.log('Streaming ended');
+                });
+            ffmpegProcess.pipe(res);
+        } else {
+            console.log("Invalid");
+            res.status("Invalid");
+        }
 
 
 
@@ -59,6 +86,9 @@ app.get('/stream/:token/:videoId', async (req, res) => {
         console.error('Error in nested API calls:', error.message);
     }
 });
+
+
+
 
 
 function verifyToken(token) {
